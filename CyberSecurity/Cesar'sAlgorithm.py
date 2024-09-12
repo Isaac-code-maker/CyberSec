965ee3480a7177dd3966ea71f578335e732a952d
Isaac-code-maker <playerredbrasil@gmail.com>
2024-09-12 10:03:57 -0300

    algoritimo de cesa substituição monoalfabetica

---
+++ CyberSecurity/Cesar'sAlgorithm.py
@@ -0,0 +1,30 @@
+import string
+import random
+
+def gerarcifra():
+    alfabeto = list(string.ascii_uppercase)
+    alfabeto_cifrado = alfabeto[:]
+
+    random.shuffle(alfabeto_cifrado)
+
+    return dict(zip(alfabeto, alfabeto_cifrado))
+
+def criptografia(texto, cifra):
+    texto_cifrado = ""
+    
+    for char in texto.upper():
+        if char in cifra:
+            texto_cifrado += cifra[char]
+        else:
+            texto_cifrado += char
+    
+    return texto_cifrado
+
+
+texto = input("Digite o texto  que deseja criptografar: ")
+
+cifra = gerarcifra()
+print("Cifra Gerada:", cifra)
+
+texto_cifrado = criptografia(texto, cifra)
+print("Texto criptografado: ", texto_cifrado)
